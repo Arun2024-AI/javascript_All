23ce22043eb4fa10c8956676e70c260bfa48fda9
Arun2024-AI <arunkumargiri@Aruns-MacBook-Pro.local>
2025-02-16 12:16:44 +0530

    new line

--- 01_varibles.js
+++ 01_varibles.js
@@ -5,3 +5,29 @@ accountCity = "Ballia"
 // accountId  = 2 // not allowed 
 
 
+var fullName = "Arun";
+
+// how to name variables
+
+// fullName
+// full-name
+// full_name
+// FullName
+
+var courseName = "React JS Bootcamp";
+
+var isLoggedIn = false;
+
+var loggedCount = 34;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
+
+// var if = 23;
+
+
