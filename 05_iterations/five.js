1791eb59da72b53e3b33cd0d37bfb731b3babb19
Arun Kumar Giri <atulgiri649@gmail.com>
2025-02-26 20:49:37 +0530

    Update five.js

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -4,9 +4,9 @@ coding.forEach( function (val){
     console.log(val);
 } )
 
-// coding.forEach( (item) => {
-//     console.log(item);
-// } )
+coding.forEach( (item) => {
+    console.log(item);
+} )
 
 // function printMe(item){
 //     console.log(item);
