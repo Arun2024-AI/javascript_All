f242d84aed15627f4653fc3c1c45f757a71c0809
Arun Kumar Giri <atulgiri649@gmail.com>
2025-03-10 22:53:56 +0530

    Update one.js

--- 04_Controls_flow/one.js
+++ 04_Controls_flow/one.js
@@ -33,10 +33,10 @@ if ( temperature === 40 ){
 // } else if (balance < 900) {
 //     console.log("less than 750");
     
-// } else {
-//     console.log("less than 1200");
+} else {
+    console.log("less than 1200");
 
-// }
+}
 
 const userLoggedIn = true
 const debitCard = true
