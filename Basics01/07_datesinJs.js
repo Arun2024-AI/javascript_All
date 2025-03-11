0c7712b7bf0c66ab9add4968cf0f25a9b6c20b8c
Arun Kumar Giri <atulgiri649@gmail.com>
2025-03-11 22:36:20 +0530

    Update 07_datesinJs.js

--- Basics01/07_datesinJs.js
+++ Basics01/07_datesinJs.js
@@ -6,9 +6,9 @@
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
